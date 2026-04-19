d6266c13118e85aa0d4fcfb1c57815fc3f284088
eagle <feraleagle@protonmail.com>
2026-04-19 13:06:12 +0500

    Environment Setup

---
+++ flight_controller/build/CMakeFiles/flight_controller.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/startup_stm32f411xe.s" "/home/eagle/programming/github_projects/quadcopter/flight_controller/build/CMakeFiles/flight_controller.dir/startup_stm32f411xe.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/cmake/stm32cubemx/../../Core/Inc"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/Core/Src/main.c" "CMakeFiles/flight_controller.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/flight_controller.dir/Core/Src/main.c.obj.d"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/Core/Src/stm32f4xx_it.c" "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/Core/Src/syscalls.c" "CMakeFiles/flight_controller.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/flight_controller.dir/Core/Src/syscalls.c.obj.d"
+  "/home/eagle/programming/github_projects/quadcopter/flight_controller/Core/Src/sysmem.c" "CMakeFiles/flight_controller.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/flight_controller.dir/Core/Src/sysmem.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ flight_controller/build/CMakeFiles/flight_controller.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "flight_controller.map"
+  "CMakeFiles/flight_controller.dir/Core/Src/main.c.obj"
+  "CMakeFiles/flight_controller.dir/Core/Src/main.c.obj.d"
+  "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/flight_controller.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "CMakeFiles/flight_controller.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/flight_controller.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/flight_controller.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/flight_controller.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/flight_controller.dir/startup_stm32f411xe.s.obj"
+  "flight_controller"
+  "flight_controller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/flight_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
